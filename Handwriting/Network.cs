6d24a38e98dae4e0607e8b571e34ec8fc5514097
Tylor Childers <tylord1@gmail.com>
2015-10-14 11:36:20 -0400

    Network constructor made
    http://neuralnetworksanddeeplearning.com/chap1.html#implementing_our_network_to_classify_digits

---
+++ Handwriting/Network.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handwriting
+{
+    class Network
+    {
+        Random r { get; set; }
+        
+        int layers { get; set; }
+        List<int> sizes { get; set; }
+        List<List<double>> biases { get; set; }
+        List<List<double>> weights { get; set; }
+
+
+        public Network(List<int> _sizes)
+        {
+            r = new Random(DateTime.Now.Millisecond);
+            sizes = _sizes;
+            biases = new List<List<double>>();
+            weights = new List<List<double>>();
+            for (int j = 0; j < sizes.Count; j++ )
+            {
+                var t = new List<double>();
+                for (int i = 0; i < sizes[j]; i++)
+                {
+                    t.Add(r.NextDouble());
+                }
+                biases.Add(t);
+                if(j+1 != sizes.Count)
+                {
+                    int x = sizes[j];
+                    int y = sizes[j + 1];
+                    for (int xi = 0; xi < x; xi++)
+                    {
+                        var temp = new List<double>();
+                        for (int yi = 0; yi < y; yi++)
+                        {
+                            temp.Add(r.NextDouble());
+                        }
+                        weights.Add(temp);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Handwriting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handwriting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = new Network(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 });
+            var l = 1 + 1;
+        }
+    }
+}
